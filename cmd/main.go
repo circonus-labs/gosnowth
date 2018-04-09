6ff91feb44fe5f81a13a0c560fa9ba71e4f21c6e
Ben Huson <ben.huson@circonus.com>
2018-04-09 13:03:31 -0400

    adding nnt and text submission apis

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"log"
+	"strconv"
+	"time"
+
+	"github.com/satori/go.uuid"
 
 	"github.com/circonus/gosnowth"
 )
 
 // main - example program that uses the snowth client
 func main() {
+	// create a client, with a seed of nodes
 	client, err := gosnowth.NewSnowthClient(
 		"http://localhost:8112",
 		"http://localhost:8113",
@@ -16,6 +21,7 @@ func main() {
 		log.Fatalf("failed to create snowth client: %v", err)
 	}
 
+	// get the node state from the node
 	for _, node := range client.ListActiveNodes() {
 		state, err := client.GetNodeState(node)
 		if err != nil {
@@ -23,6 +29,7 @@ func main() {
 		}
 		log.Printf("%+v", state)
 	}
+	// get the gossip data from the node
 	for _, node := range client.ListActiveNodes() {
 		gossip, err := client.GetGossipInfo(node)
 		if err != nil {
@@ -30,6 +37,7 @@ func main() {
 		}
 		log.Printf("%+v", gossip)
 	}
+	// get the topology from the node
 	for _, node := range client.ListActiveNodes() {
 		topology, err := client.GetTopologyInfo(node)
 		if err != nil {
@@ -37,11 +45,51 @@ func main() {
 		}
 		log.Printf("%+v", topology)
 	}
+	// get the toporing from the node
 	for _, node := range client.ListActiveNodes() {
 		toporing, err := client.GetTopoRingInfo(node.GetCurrentTopology(), node)
 		if err != nil {
 			log.Fatalf("failed to get topology: %v", err)
 		}
 		log.Printf("%+v", toporing)
+	}
+	// write nnt data to node
+	for _, node := range client.ListActiveNodes() {
+		guid, _ := uuid.NewV4()
+
+		err := client.WriteNNT(
+			[]gosnowth.NNTData{
+				gosnowth.NNTData{
+					Metric: "test-metric", ID: guid.String(),
+					Offset: time.Now().Unix(),
+					Count:  5, Value: 100,
+					Parts: gosnowth.Parts{
+						Period: 60,
+						Data: []gosnowth.NNTPartsData{
+							gosnowth.NNTPartsData{Count: 1, Value: 100},
+							gosnowth.NNTPartsData{Count: 1, Value: 100},
+							gosnowth.NNTPartsData{Count: 1, Value: 100},
+							gosnowth.NNTPartsData{Count: 1, Value: 100},
+							gosnowth.NNTPartsData{Count: 1, Value: 100},
+						},
+					},
+				}}, node)
+		if err != nil {
+			log.Fatalf("failed to write nnt data: %v", err)
+		}
+	}
+	// write text data
+	for _, node := range client.ListActiveNodes() {
+		guid, _ := uuid.NewV4()
+		err := client.WriteText(
+			[]gosnowth.TextData{
+				gosnowth.TextData{
+					Metric: "test-text-metric2", ID: guid.String(),
+					Offset: strconv.FormatInt(time.Now().Unix(), 10),
+					Value:  "a_text_data_value",
+				}}, node)
+		if err != nil {
+			log.Fatalf("failed to write text data: %v", err)
+		}
 	}
 }
