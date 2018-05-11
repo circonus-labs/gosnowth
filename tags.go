b99258f41ca5a75072a3b371d99258db7999ff6b
Ben Huson <ben.huson@circonus.com>
2018-05-11 13:23:10 -0400

    cleaning up tag search

--- tags.go
+++ tags.go
@@ -1,16 +1,9 @@
 package gosnowth
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/url"
-
-	"github.com/pkg/errors"
 )
-
-type FindTagsResponse []FindTagsItem
 
 type FindTagsItem struct {
 	UUID       string
@@ -22,33 +15,14 @@ type FindTagsItem struct {
 }
 
 // FindTags - Find metrics that are associated with tags
-func (sc *SnowthClient) FindTags(node *SnowthNode, accountID int32, query string) (FindTagsResponse, error) {
+func (sc *SnowthClient) FindTags(node *SnowthNode, accountID int32, query string) ([]FindTagsItem, error) {
 	url := fmt.Sprintf("%s?query=%s",
 		sc.getURL(node, fmt.Sprintf("/find/%d/tags", accountID)),
 		url.QueryEscape(query),
 	)
-
-	r, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create request")
-	}
-	resp, err := sc.c.Do(r)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to perform request")
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		body, _ := ioutil.ReadAll(resp.Body)
-		return nil, fmt.Errorf("non-success status code returned: %s -> %s",
-			resp.Status, string(body))
-	}
-
-	var ftr = FindTagsResponse{}
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&ftr); err != nil {
-		return nil, errors.Wrap(err, "failed to decode json response body")
-	}
-
-	return ftr, nil
+	var (
+		r   = []FindTagsItem{}
+		err = sc.do(node, "GET", url, nil, &r, decodeJSONFromResponse)
+	)
+	return r, err
 }
