d11d8ac195dd2e75273245a94fed5c17319cfc9d
David Haifley <dhaifley@gmail.com>
2021-02-17 22:43:27 -0500

    CIRC-3442: Improve test coverage

--- text_test.go
+++ text_test.go
@@ -20,6 +20,53 @@ func TestTextValue(t *testing.T) {
 	}
 }
 
+func TestReadTextValuesFindMetricNode(t *testing.T) {
+	ms := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter,
+		r *http.Request) {
+		if r.RequestURI == "/state" {
+			_, _ = w.Write([]byte(stateTestData))
+			return
+		}
+
+		if r.RequestURI == "/stats.json" {
+			_, _ = w.Write([]byte(statsTestData))
+			return
+		}
+
+		if strings.HasPrefix(r.RequestURI,
+			"/read/1/2/3aa57ac2-28de-4ec4-aa3d-ed0ddd48fa4d/test") {
+			_, _ = w.Write([]byte(textTestData))
+			return
+		}
+
+		w.WriteHeader(500)
+	}))
+
+	defer ms.Close()
+	sc, err := NewSnowthClient(false, ms.URL)
+	if err != nil {
+		t.Fatal("Unable to create snowth client", err)
+	}
+
+	res, err := sc.ReadTextValues("3aa57ac2-28de-4ec4-aa3d-ed0ddd48fa4d",
+		"test", time.Unix(1, 0), time.Unix(2, 0))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(res) != 2 {
+		t.Fatalf("Expected result length: 2, got: %v", len(res))
+	}
+
+	if res[0].Value == nil {
+		t.Fatal("Expected value: not nil, got: nil")
+	}
+
+	if *res[0].Value != "hello" {
+		t.Errorf("Expected value: hello, got: %v", *res[0].Value)
+	}
+}
+
 func TestReadTextValues(t *testing.T) {
 	ms := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter,
 		r *http.Request) {
